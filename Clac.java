5b86804efe84f0cc7ddc58ff18bd4b3fae554085
mohammadgaus <mohammadkaud5@gmail.com>
2025-07-30 09:43:40 +0530

    Create Clac.java

---
+++ Clac.java
@@ -0,0 +1,15 @@
+class Calc{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a num1:");
+        int n1=sc.nextInt();
+        System.out.println("Enter a operator(+,-,*,/):");
+        char ch=sc.next();
+        System.out.println("Enter a num2:");
+        int n2=sc.nextInt();
+        switch(ch){
+            case +:
+                System.out.println("n1+n2=",+n1+n2);
+        }
+    } 
+}
