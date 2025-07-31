ea147b8f11e3e54bbbeb007a89960ba69f6bca5a
mohammadgaus <mohammadkaud5@gmail.com>
2025-07-31 09:45:16 +0530

    Create arraysearch.java

---
+++ arraysearch.java
@@ -0,0 +1,13 @@
+class Array{
+    public static void main(String arg[]){
+        Scanner sc=new Scanner(System.in);
+        int choice;
+        while(true){
+            System.out.println("\nMenu:");
+            System.out.println("1-Stores values in array");
+            System.out.println("2-Search value");
+            System.out.println("3-Exit");
+            
+        }
+    }
+}
